a860dbc140de467e8226be2354120045f11721a5
Stephen Chapman <schapm16@gmail.com>
2017-11-30 23:18:10 +0000

    Spotify response data printed in the required format.

--- keys.js
+++ keys.js
@@ -7,8 +7,15 @@ var twitterKeys = {
   access_token_secret: 'lRjO5Kv93xpc0TgRTFIO2TC5l8bKzri6xA1U8BAsUYi1g',
 };
 
+var spotifyKeys = {
+  id: 'fd74938b48d74a0ebfc13f7e69d6d7ea',
+  secret: '9c4bdcd4dd4d42c59c23b22f80427497'
+};
 
-module.exports = twitterKeys;
+module.exports = {
+  twitterKeys: twitterKeys,
+  spotifyKeys: spotifyKeys
+};
 
 //Spofity fd74938b48d74a0ebfc13f7e69d6d7ea
 // 9c4bdcd4dd4d42c59c23b22f80427497
--- liri.js
+++ liri.js
@@ -1,15 +1,17 @@
 // Incorporate modules
 var request = require('request');
 
-var twitterKeys = require('./keys.js');
+var keys = require('./keys.js'); // Contains keys for Twitter and Spotify
 var Twitter = require('twitter');
 
 var Spotify = require('node-spotify-api');
 // ---------------------------------------
 
+
 // The user's raw input
 const rawInput = process.argv;
 // ---------------------------------------
+
 
 // Function to parse user input
 function parse() {
@@ -27,7 +29,7 @@ function parse() {
 // Function to make Twitter API call and display results
 function twitterAPI() {
 
-  var client = new Twitter(twitterKeys);
+  var client = new Twitter(keys.twitterKeys);
 
   var params = {
     screen_name: 'schapm16',
@@ -39,11 +41,11 @@ function twitterAPI() {
       for (var i = 0; i < tweets.length; i++) {
         var time = tweets[i].created_at;
         var text = tweets[i].text;
-        console.log('Posted At: ' + time + '    ' + 'Post Content:' + text);
+        console.log('Posted At: ' + time + '    ' + 'Post Content: ' + text);
       }
     }
     else {
-      console.log('Uh Oh! Something went wrong with your request for Tweets. \n\nError Details:\n' + error);
+      console.log('Error Occured:\n' + error);
     }
   });
 }
@@ -55,21 +57,28 @@ function twitterAPI() {
 function spotifyAPI(parsedInput) {
 
   if (!parsedInput.length) {
-    parsedInput = "The sign";
+    parsedInput = '"The sign"';
+  }
+  else {
+    parsedInput = '"' + parsedInput + '"'; // Add double quotes per spotify API to search by keywords in the specific order entered by user
   }
 
-  var spotify = new Spotify({
-    id: 'fd74938b48d74a0ebfc13f7e69d6d7ea',
-    secret: '9c4bdcd4dd4d42c59c23b22f80427497',
-  });
+  var spotify = new Spotify(keys.spotifyKeys);
 
   spotify.search({ type: 'track', query: parsedInput, market: "from_token", limit: "1" }, function(err, data) {
     if (err) {
-      return console.log('Error occurred: ' + err);
+      return console.log('Error occurred:\n' + err);
     }
     else {
-      // console.log(JSON.stringify(data, null, 2));
-      console.log(data.tracks.items);
+      var dataShort = data.tracks.items[0]; //Variable to store common path into JSON response
+      console.log('Top Result for: ' + parsedInput + '\n');
+      console.log(
+        'Artist: ' + dataShort.artists[0].name +
+        '\nSong Name: ' + dataShort.name +
+        '\nPreview: ' + dataShort.preview_url +
+        '\nAlbum: ' + dataShort.album.name
+
+      );
     }
   });
 }
@@ -78,7 +87,7 @@ function spotifyAPI(parsedInput) {
 // Function to make OMDB API call using Request npm package
 
 function omdbAPI() {
-  
+
 }
 
 //-------------------------------------------
